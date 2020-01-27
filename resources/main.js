4c3af19b561c1bfcf903a94f8da9102fe9e04dbb
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2020-01-27 09:28:26 +0200

    added banner styles and close buttons

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,15 @@
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu-container");
+const closeButtonList = document.querySelectorAll(".banner-close");
 
-menuButton.addEventListener("click", function () {
+
+menuButton.addEventListener("click", () => {
     menu.classList.toggle("hidden-right");
+});
+
+
+closeButtonList.forEach(element => {
+    element.addEventListener("click", () => {
+        element.parentElement.classList.add("dismissed");
+    });
 });
