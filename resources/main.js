8c06ae5754f3b63dad492c7d73f68887d90eaba6
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2020-01-28 11:17:14 +0200

    added footer added info to about section enabled analytics

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,8 @@
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu-container");
-const closeButtonList = document.querySelectorAll(".banner-close");
-
+const closeButtonList = document.querySelectorAll(".close-button");
+const currentYear = document.querySelector("#current-year");
+const today = Date();
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("hidden-right");
@@ -13,3 +14,5 @@ closeButtonList.forEach(element => {
         element.parentElement.classList.add("dismissed");
     });
 });
+
+currentYear.innerHTML = today.getFullYear().toString();
