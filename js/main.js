6fec7795c397b51be1f20479514b6e12cf4ea512
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-12-04 16:23:24 +0200

    added project descriptions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,36 @@
-const header = document.querySelector('header');
-const leftContainer = document.querySelector('#left-container');
-const rightContainer = document.querySelector('#right-container');
+'use strict';
 
 const consentContainer = document.querySelector('#cookie-consent');
 const consentCloseButton = document.querySelector('#consent-close');
 
-const navigationList = document.querySelector('#navigation-list');
-const headers = document.querySelectorAll('#content-container h2,' +
-	'#content-container h3,' +
-	'#content-container h4');
+const navigationList = document.querySelector('#nav-list');
+const headers = document.querySelectorAll('article h2,' +
+		'article h3,' +
+		'article h4');
+
+
+function closeConsent() {
+	consentContainer.parentNode.removeChild(consentContainer);
+}
 
 
 consentCloseButton.addEventListener('click', e => {
-	consentContainer.parentNode.removeChild(consentContainer);
+	localStorage.setItem('consent-closed', 'true');
+	closeConsent();
 });
 
 
 addEventListener('load', e => {
+	if (localStorage.getItem('consent-closed')) {
+		closeConsent();
+	}
+
 	headers.forEach((header) => {
-		const headerLI = document.createElement('li');
-		navigationList.appendChild(headerLI);
 		const headerLink = document.createElement('a');
 		headerLink.href = '#' + header.id;
-		headerLI.appendChild(headerLink);
 		const headerElement = document.createElement(header.tagName);
 		headerElement.innerHTML = header.innerHTML;
+		navigationList.appendChild(headerLink);
 		headerLink.appendChild(headerElement);
 	});
 });
