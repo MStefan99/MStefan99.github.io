efeeecaf99c6100fad203802c8e597beb22ef188
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-11-23 21:33:40 +0200

    updated cv

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
 'use strict';
 
-const consentContainer = document.querySelector('#cookie-consent');
-const consentCloseButton = document.querySelector('#consent-close');
-
 const navigationList = document.querySelector('#nav-list');
 const headers = document.querySelectorAll('article h2,' +
 		'article h3,' +
 		'article h4');
 
-
-function closeConsent() {
-	consentContainer.parentNode.removeChild(consentContainer);
-}
-
-
-consentCloseButton.addEventListener('click', e => {
-	localStorage.setItem('consent-closed', 'true');
-	closeConsent();
-});
-
-
 addEventListener('load', e => {
-	if (localStorage.getItem('consent-closed')) {
-		closeConsent();
-	}
-
 	headers.forEach((header) => {
 		const headerLink = document.createElement('a');
 		headerLink.href = '#' + header.id;
