49ce5ae915cbc211ea72a01b297ec80a23fbefee
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-11-28 18:20:14 +0200

    added image compression and metadata stripping

--- copy-files.js
+++ copy-files.js
@@ -4,10 +4,36 @@ import path from 'node:path';
 import fs from 'node:fs/promises'
 import {fileURLToPath} from 'url';
 
+import sharp from 'sharp';
 import argumented from '@mstefan99/argumented';
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const SIZE_THRESHOLD = 512 * 1024; // 0.5MB image size threshold
+
+
+function isImageFile(filePath) {
+	const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.tiff'];
+	return imageExtensions.some(ext => filePath.endsWith(ext));
+}
+
+async function removeMetadata(filePath) {
+	await sharp(filePath)
+		// This will correctly rotate the image and strip metadata
+		.rotate()
+		.toBuffer()
+		/* Rewriting the image in place to strip metadata will cause nodemon to restart the process
+		 * when an image is initially copied into the project, but I don't have a better solution yet
+		 */
+		.then(buffer => fs.writeFile(filePath, buffer));
+}
+
+async function compressAndCopyImage(srcPath, destPath) {
+	await sharp(srcPath)
+		.resize({width: 1920, withoutEnlargement: true})
+		.jpeg({quality: 70})
+		.toFile(destPath);
+}
 
 async function copyFileIfNewer(src, dest) {
 	const srcStat = await fs.stat(src);
@@ -23,13 +49,25 @@ async function copyFileIfNewer(src, dest) {
 	}
 
 	if (newer) {
+		if (isImageFile(src)) {
+			// TODO: fix script restarting itself
+			// await removeMetadata(src);
+			if (srcStat.size > SIZE_THRESHOLD) {
+				await compressAndCopyImage(src, dest);
+			} else {
+				await fs.cp(src, dest);
+			}
+		} else {
+			await fs.cp(src, dest);
+		}
 		console.log('[File copy]', src, '->', dest);
-		await fs.cp(src, dest);
 	}
 }
 
 async function copyFiles(src, dest) {
-	try {
+	const stat = await fs.stat(src);
+
+	if (stat.isDirectory()) {
 		const entries = await fs.readdir(src, {withFileTypes: true});
 
 		for (const entry of entries) {
@@ -37,12 +75,13 @@ async function copyFiles(src, dest) {
 			const destPath = path.join(dest, entry.name);
 
 			if (entry.isDirectory()) {
+				await fs.mkdir(destPath, {recursive: true});
 				await copyFiles(srcPath, destPath)
 			} else {
 				await copyFileIfNewer(srcPath, destPath)
 			}
 		}
-	} catch {
+	} else {
 		await copyFileIfNewer(src, dest);
 	}
 }
