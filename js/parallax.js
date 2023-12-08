5e053556f352dbce58abe8057e03be2ff514755d
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-12-08 19:45:39 +0000

    added snapping

--- js/parallax.js
+++
@@ -1,9 +0,0 @@
-
-(() => {
-	const header = document.querySelector('header');
-
-	header.oncontextmenu = e => {
-		e.preventDefault();
-		document.querySelector('.parallax').classList.toggle('debug')
-	}
-})();
